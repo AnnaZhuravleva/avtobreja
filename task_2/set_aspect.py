cfedd612b1343e076f06b34eaffb9a597f8e83d8
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2019-12-24 02:17:38 +0300

    updates

--- task_2/set_aspect.py
+++ task_2/set_aspect.py
@@ -176,18 +176,54 @@ def set_score(food, service, food_b, service_b, ngram):
     return result
 
 
-def find_aspect(filepath, food_path, serv_path):
+def check_list(word, food_list, service_list):
+    with open(food_list, 'r', encoding='utf-8') as f:
+        food = list(f.read().split('\n'))
+    with open(service_list, 'r', encoding='utf-8') as f:
+        service = list(f.read().split('\n'))
+    result = None
+    if word in food:
+        result = 'food'
+    elif word in service:
+        result = 'service'
+    return result
+
+
+def define_score(ngram, grams, food, service):
+    head = ngram[0][1] if isinstance(ngram[0], list) else ngram[0]
+    ngrams = [g for g in grams if isinstance(g[0], list)]
+    if isinstance(ngram[0], str):
+        nb = ngram[0]
+        grams = [t[0][1] for t in ngrams if nb in [a[0] for a in t]]
+    else:
+        nbs = [i[0] for i in ngram]
+        grams = []
+        for g in ngrams:
+            nbs2 = [a[0] for a in g]
+            if all(map(lambda x: x in nbs2, nbs)) and g != ngram:
+                grams.append(g[0][1])
+    grams = set([i.split('_')[0] for i in grams]) if len(grams) > 0 else head
+    for word in grams:
+        ch = check_list(word, food, service)
+        if ch:
+            return ch
+    return None
+
+
+def find_aspect(filepath, food_path, serv_path, food_keys, service_keys):
     '''
     :param filepath: path to conllu file (file should end with one single line)
     :param food_path: path to file with weights of food words
     :param serv_path: path to file with weights of service words
+    :param food_keys: path to file with keys for food
+    :param service_keys: path to file with keys for service
     :return: None, result in ./file score.txt
     '''
     with open(food_path, 'r', encoding='utf-8') as ff:
         reader = csv.reader(ff, delimiter='\t')
         food = {i[0].lower(): i[1] for i in reader}
         food_b = {tuple(set(i[0].split(' '))): i[1] for i in food.items() if
-                  len( i[0].split(' ')) > 1}
+                  len(i[0].split(' ')) > 1}
     with open(serv_path, 'r', encoding='utf-8') as fs:
         reader = csv.reader(fs, delimiter='\t')
         service = {i[0].lower(): i[1] for i in reader}
@@ -202,13 +238,24 @@ def find_aspect(filepath, food_path, serv_path):
             score = set_score(food, service, food_b, service_b, i)
             if len(score) > 1:
                 with open('score.txt', 'a', encoding='utf-8') as fo:
-                    res = str(idx) + '\t' + score[0] + '\t' + score[1]
+                    res = str(idx) + '\t' + score[0] + '\t'
                     if len(score) == 3:
-                        res += '\t' + score[2]
+                        defsc = define_score(i, grams, food_keys, service_keys)
+                        if defsc == 'food':
+                            res += (score[1], score[2])['food' in score[2]]
+                        elif defsc == 'service':
+                            res += (score[1], score[2])['food' in score[1]]
+                        elif '1' in score[2] or '1' in score[1]:
+                            res += (score[1], score[2])['1' in score[2]]
+                        else:
+                            res += score[1]
+                    if len(score) == 2:
+                        res += score[1]
                     fo.write(res)
                     fo.write('\n')
 
 
 if __name__ == '__main__':
-    find_aspect('test.conllu', 'food_with_3gramms.csv',
-                'service_with_3gramms.csv')
+    find_aspect('example/test.conllu', 'food_with_3gramms.csv',
+                'service_with_3gramms.csv', 'final_food_keys.txt',
+                'service_key_extended.txt')
